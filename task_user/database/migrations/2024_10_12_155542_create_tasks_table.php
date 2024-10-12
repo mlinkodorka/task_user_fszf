919ac2b56d1bc825115f034eea5a70aefa39e013
mlinkodorka <mlinkodorottyavirag@gmail.com>
2024-10-12 18:20:04 +0200

    end_of_class_241012

---
+++ task_user/database/migrations/2024_10_12_155542_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use League\CommonMark\Reference\Reference;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('discription');
+            $table->date('end_date')->default('2024-10-12');
+            $table->foreignId('user_id')->references('id')->on('users');
+            // 0: nincs kész, 1: kész van
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
